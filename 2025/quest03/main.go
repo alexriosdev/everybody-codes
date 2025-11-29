581049d98c704e3a8f7dc1a28e2991e1ede5e1cc
Alex Rios <alexrios.dev@gmail.com>
2025-11-29 15:24:41 -0600

    feat(2025): complete quest03 part2

--- 2025/quest03/main.go
+++ 2025/quest03/main.go
@@ -3,25 +3,46 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 func main() {
 	input1, _ := os.ReadFile("2025/quest03/input1.txt")
+	input2, _ := os.ReadFile("2025/quest03/input2.txt")
 	fmt.Println("2025 Quest 03 Solution")
 	fmt.Printf("Part 1: %v\n", part1(input1))
+	fmt.Printf("Part 2: %v\n", part2(input2))
 }
 
 func part1(input []byte) int {
-	split := strings.Split(string(input), ",")
-	uniqueNums := map[int]bool{}
+	split := strings.Split(strings.TrimSpace(string(input)), ",")
+	set := map[int]bool{}
 	for _, s := range split {
-		uniqueNums[StrToInt(s)] = true
+		set[StrToInt(s)] = true
 	}
 	sum := 0
-	for num := range uniqueNums {
+	for num := range set {
 		sum += num
+	}
+	return sum
+}
+
+func part2(input []byte) int {
+	split := strings.Split(strings.TrimSpace(string(input)), ",")
+	set := map[int]bool{}
+	for _, s := range split {
+		set[StrToInt(s)] = true
+	}
+	uniqueNums := []int{}
+	for num := range set {
+		uniqueNums = append(uniqueNums, num)
+	}
+	sort.Ints(uniqueNums)
+	sum := 0
+	for i := 0; i < 20; i++ {
+		sum += uniqueNums[i]
 	}
 	return sum
 }
--- 2025/quest03/main_test.go
+++ 2025/quest03/main_test.go
@@ -23,9 +23,34 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	input1, _ := os.ReadFile("input_test2.txt")
+	input2, _ := os.ReadFile("input2.txt")
+	tests := []struct {
+		input    []byte
+		expected int
+	}{
+		{input1, 781},
+		{input2, 213},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	input, _ := os.ReadFile("input1.txt")
 	for n := 0; n < b.N; n++ {
 		part1(input)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	input, _ := os.ReadFile("input2.txt")
+	for n := 0; n < b.N; n++ {
+		part2(input)
 	}
 }
