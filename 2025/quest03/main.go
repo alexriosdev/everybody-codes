ed95b12ee96b8d324650e98521aaedca897e2384
Alex Rios <alexrios.dev@gmail.com>
2025-11-29 16:00:17 -0600

    feat(2025): complete quest03 part3

--- 2025/quest03/main.go
+++ 2025/quest03/main.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	input1, _ := os.ReadFile("2025/quest03/input1.txt")
 	input2, _ := os.ReadFile("2025/quest03/input2.txt")
+	input3, _ := os.ReadFile("2025/quest03/input3.txt")
 	fmt.Println("2025 Quest 03 Solution")
 	fmt.Printf("Part 1: %v\n", part1(input1))
 	fmt.Printf("Part 2: %v\n", part2(input2))
+	fmt.Printf("Part 3: %v\n", part3(input3))
 }
 
 func part1(input []byte) int {
@@ -45,6 +47,19 @@ func part2(input []byte) int {
 		sum += uniqueNums[i]
 	}
 	return sum
+}
+
+func part3(input []byte) int {
+	split := strings.Split(strings.TrimSpace(string(input)), ",")
+	freq := map[int]int{}
+	for _, s := range split {
+		freq[StrToInt(s)]++
+	}
+	count := 0
+	for _, v := range freq {
+		count = max(count, v)
+	}
+	return count
 }
 
 func StrToInt(s string) int {
