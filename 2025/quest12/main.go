cf269cf7231e549184ebf8859860d2e10a4d0eb3
Alex Rios <alexrios.dev@gmail.com>
2025-12-21 00:34:36 -0600

    feat(2025): complete quest12 part2

--- 2025/quest12/main.go
+++ 2025/quest12/main.go
@@ -17,41 +17,56 @@ func main() {
 
 func part1(lines []string) int {
 	grid := NewGrid(lines)
-	dirs := []Coordinate{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
 	start := Coordinate{0, 0}
 	queue := []Coordinate{start}
 	visited := map[Coordinate]bool{}
 	count := 0
 	for len(queue) > 0 {
-		size := len(queue)
-		for i := 0; i < size; i++ {
-			curr := queue[0]
-			queue = queue[1:]
-			if visited[curr] {
-				continue
-			}
-			visited[curr] = true
-			count++
-			for _, dir := range dirs {
-				next := Coordinate{curr.Y + dir.Y, curr.X + dir.X}
-				if !grid.IsInBounds(next) {
-					continue
-				}
-				if grid.IsGreaterOrEqual(curr, next) {
-					queue = append(queue, next)
-				}
-			}
-		}
+		igniteBarrels(&queue, grid, &visited, &count)
 	}
 	return count
 }
 
 func part2(lines []string) int {
-	return len(lines)
+	grid := NewGrid(lines)
+	start := Coordinate{0, 0}
+	end := Coordinate{grid.Rows() - 1, grid.Cols() - 1}
+	startQueue := []Coordinate{start}
+	endQueue := []Coordinate{end}
+	visited := map[Coordinate]bool{}
+	count := 0
+	for len(startQueue) > 0 && len(endQueue) > 0 {
+		igniteBarrels(&startQueue, grid, &visited, &count)
+		igniteBarrels(&endQueue, grid, &visited, &count)
+	}
+	return count
 }
 
 func part3(lines []string) int {
 	return len(lines)
+}
+
+func igniteBarrels(queue *[]Coordinate, grid *Grid, visited *map[Coordinate]bool, count *int) {
+	dirs := []Coordinate{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+	size := len(*queue)
+	for i := 0; i < size; i++ {
+		curr := (*queue)[0]
+		(*queue) = (*queue)[1:]
+		if (*visited)[curr] {
+			continue
+		}
+		(*visited)[curr] = true
+		*count++
+		for _, dir := range dirs {
+			next := Coordinate{curr.Y + dir.Y, curr.X + dir.X}
+			if !grid.IsInBounds(next) {
+				continue
+			}
+			if grid.IsGreaterOrEqual(curr, next) {
+				*queue = append(*queue, next)
+			}
+		}
+	}
 }
 
 type Coordinate struct {
@@ -68,10 +83,18 @@ func NewGrid(lines []string) *Grid {
 	return grid
 }
 
+func (g *Grid) Rows() int {
+	return len(*g)
+}
+
+func (g *Grid) Cols() int {
+	return len((*g)[0])
+}
+
 func (g *Grid) IsGreaterOrEqual(a, b Coordinate) bool {
 	return (*g)[a.Y][a.X] >= (*g)[b.Y][b.X]
 }
 
 func (g *Grid) IsInBounds(c Coordinate) bool {
-	return 0 <= c.Y && c.Y < len(*g) && 0 <= c.X && c.X < len((*g)[0])
+	return 0 <= c.Y && c.Y < g.Rows() && 0 <= c.X && c.X < g.Cols()
 }
